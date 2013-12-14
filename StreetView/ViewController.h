c0cb28f3b0554c90c53be09a1cb2aa8e7a8fc90f
jpatrickburns <jpburns@gmail.com>
2013-12-14 11:50:37 -0500

    Initial Commit

---
+++ StreetView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StreetView
+//
+//  Created by James Burns on 12/14/13.
+//  Copyright (c) 2013 James Burns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ StreetViewTests/StreetViewTests.m
@@ -0,0 +1,34 @@
+//
+//  StreetViewTests.m
+//  StreetViewTests
+//
+//  Created by James Burns on 12/14/13.
+//  Copyright (c) 2013 James Burns. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StreetViewTests : XCTestCase
+
+@end
+
+@implementation StreetViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
