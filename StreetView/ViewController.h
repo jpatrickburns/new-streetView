cf73a439fe64bee1fd5aec902299a65d2457fbaf
jpatrickburns <jpburns@gmail.com>
2013-12-14 13:32:40 -0500

    Fresh start on this - working buttons

--- StreetView/ViewController.h
+++ StreetView/ViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+- (IBAction)changeScene:(id)sender;
 
 @end
--- StreetView/ViewController.m
+++ StreetView/ViewController.m
@@ -26,4 +26,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changeScene:(id)sender {
+    if ([sender isMemberOfClass:[UIButton class]]) {
+        
+        UIButton *btn = (UIButton *)sender;
+        if ([btn.currentTitle isEqualToString:@"Historical"]) {
+            NSLog(@"Pushed historical");
+        }else if ([btn.currentTitle isEqualToString:@"Attractions"]){
+            NSLog(@"Pushed Attractions");
+        }else if ([btn.currentTitle isEqualToString:@"Neighborhoods"]){
+            NSLog(@"Pushed neighborhoods");
+        }else if ([btn.currentTitle isEqualToString:@"Only in the ATL"]){
+            NSLog(@"Pushed Only in the ATL");
+        }else if ([btn.currentTitle isEqualToString:@"What's Near Me?"]){
+            NSLog(@"Pushed What's Near Me?");
+        }else if ([btn.currentTitle isEqualToString:@"Special Offers"]){
+            NSLog(@"Pushed Special Offers");
+        }else{
+            NSLog(@"Something went wrong.");
+        }
+    }
+         }
 @end
